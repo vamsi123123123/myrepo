0526941d9dd1809ac5d8d9c967ccfa65276f5b90
vamsi123123123 <vamsinaiduvemula2828@gmail.com>
2026-03-14 23:52:02 +0530

    Update app.java

--- app.java
+++ app.java
@@ -16,3 +16,8 @@ public class AddNumbers {
         System.out.println("Sum = " + sum);
     }
 }
+
+
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+/usr/share/keyrings/jenkins-keyring.asc > /dev/null
